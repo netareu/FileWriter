cd85bba8225923f976cac63c5c6933ba6cb5b41b
Neta Reuveni <netareuveni122@gmail.com>
2025-06-29 14:16:27 +0300

    FileWriter2

---
+++ FileWriter/FileWriter/AppConfigImageFinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Security.Principal;
+using static System.Net.Mime.MediaTypeNames.Image;
+
+namespace FileWriter
+{
+    public class AppConfigImageFinder : BaseFileWriter
+    {
+        private Image _image;
+        public AppConfigImageFinder():base()
+        {
+            _image = Image.FromFile(ConfigurationManager.AppSettings["ImagePath"]);
+            _path = ConfigurationManager.AppSettings["FilePath"];
+            base.WriteLine($"{_image.Height}X{_image.Width}");
+        }
+    }
+}
--- FileWriter/FileWriter/Program.cs
+++ FileWriter/FileWriter/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using FileWriter;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Security.Principal;
+using static System.Net.Mime.MediaTypeNames.Image;
+
+AppConfigImageFinder appConfigImageFinder = new AppConfigImageFinder();
